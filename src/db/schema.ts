f695773466b145962385d111558c9e0dcdb724a9
Shubham Yadav <shubhamya.1157@gmail.com>
2026-07-05 10:32:54 +0530

    making auth basic auth schema in postgres

---
+++ src/db/schema.ts
@@ -0,0 +1,35 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  timestamp,
+  boolean,
+  text,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  firstName: varchar("first_name", { length: 10 }).notNull(),
+
+  lastName: varchar("last_name", { length: 10 }),
+
+  email: varchar("email", { length: 322 })
+    .notNull()
+    .unique(),
+
+  emailVerified: boolean("email_verified")
+    .default(false)
+    .notNull(),
+
+  password: varchar("password", { length: 66 }),
+
+  salt: text("salt"),
+
+  createdAt: timestamp("created_at")
+    .defaultNow()
+    .notNull(),
+
+  updatedAt: timestamp("updated_at")
+    .$onUpdate(() => new Date()),
+});
